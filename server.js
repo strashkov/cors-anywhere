ed568c0165b2488dce57d955a848ecfccc522df4
Aleksandr Strashkov <alstrashkov@gmail.com>
2024-10-21 10:40:28 +0200

    In-memory cache added and used when failed to fetch json file

--- server.js
+++ server.js
@@ -18,12 +18,14 @@ function parseEnvList(env) {
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
 var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
+const cacheJson = process.env.CORSANYWHERE_CACHE_JSON;
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
+  cacheJson,
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: [],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
